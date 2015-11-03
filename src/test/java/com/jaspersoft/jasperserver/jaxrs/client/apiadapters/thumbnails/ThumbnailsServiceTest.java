97b9ce250bef73736619dc1268323550aa0c6706
TanyaEf <efimenkot@gmial.com>
2015-11-03 16:07:48 +0200

    New test for new error handling strategy

--- src/test/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/thumbnails/ThumbnailsServiceTest.java
+++ src/test/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/thumbnails/ThumbnailsServiceTest.java
@@ -3,15 +3,21 @@ package com.jaspersoft.jasperserver.jaxrs.client.apiadapters.thumbnails;
 import com.jaspersoft.jasperserver.dto.thumbnails.ResourceThumbnail;
 import com.jaspersoft.jasperserver.jaxrs.client.RestClientTestUtil;
 import com.jaspersoft.jasperserver.jaxrs.client.core.enums.RequestMethod;
+import com.jaspersoft.jasperserver.jaxrs.client.core.exceptions.RequestedRepresentationNotAvailableForResourceException;
+import com.jaspersoft.jasperserver.jaxrs.client.core.exceptions.ResourceNotFoundException;
+import com.jaspersoft.jasperserver.jaxrs.client.core.operationresult.OperationResult;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
-import org.testng.Assert;
+import javax.ws.rs.core.Response;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static java.util.Arrays.asList;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.AssertJUnit.assertTrue;
 
 /**
 * Integration tests for {@link ThumbnailsService}
@@ -29,6 +35,7 @@ public class ThumbnailsServiceTest extends RestClientTestUtil {
      * Batch thumbnails operation
      */
     public void should_return_list_of_thumbnails_with_default_request_method() {
+        // When
         List<ResourceThumbnail> entity = session.thumbnailsService()
                 .thumbnails()
                 .reports("/public/Samples/Reports/08g.UnitSalesDetailReport",
@@ -37,8 +44,9 @@ public class ThumbnailsServiceTest extends RestClientTestUtil {
                 .get()
                 .getEntity()
                 .getThumbnails();
-        Assert.assertNotNull(entity);
-        Assert.assertTrue(entity.size() == 2);
+        // Then
+        assertNotNull(entity);
+        assertTrue(entity.size() == 2);
     }
 
     @Test
@@ -46,6 +54,7 @@ public class ThumbnailsServiceTest extends RestClientTestUtil {
      * Batch thumbnails operation
      */
     public void should_return_list_of_thumbnails_with_get_request_method() {
+        // When
         List<ResourceThumbnail> entity = session.thumbnailsService()
                 .thumbnails()
                 .reports(asList("/public/Samples/Reports/08g.UnitSalesDetailReport",
@@ -54,8 +63,9 @@ public class ThumbnailsServiceTest extends RestClientTestUtil {
                 .get()
                 .getEntity()
                 .getThumbnails();
-        Assert.assertNotNull(entity);
-        Assert.assertTrue(entity.size() == 2);
+        // Then
+        assertNotNull(entity);
+        assertTrue(entity.size() == 2);
     }
 
     @Test
@@ -63,13 +73,106 @@ public class ThumbnailsServiceTest extends RestClientTestUtil {
      * Single thumbnail operation
      */
     public void should_return_single_thumbnail_as_stream() throws IOException {
+        // When
         InputStream entity = session.thumbnailsService()
                 .thumbnail()
                 .report("/public/Samples/Reports/08g.UnitSalesDetailReport")
                 .defaultAllowed(true)
                 .get()
                 .getEntity();
-        Assert.assertNotNull(entity);
+        // Then
+        assertNotNull(entity);
+    }
+
+    @Test(expectedExceptions = ResourceNotFoundException.class)
+    public void should_throw_ResourceNotFoundException() throws IOException {
+        // Given
+        configuration.setHandleErrors(true);
+        // When
+        session.thumbnailsService()
+                .thumbnail()
+                .report("/some")
+                .get();
+        // Then an exception should be thrown
+    }
+
+
+    @Test
+    public void should_not_throw_ResourceNotFoundException_and_return_null_entity() throws IOException {
+        // Given
+        configuration.setHandleErrors(false);
+        // When
+        OperationResult<InputStream> result = session
+                .thumbnailsService()
+                .thumbnail()
+                .report("/some")
+                .get();
+        // Then
+        assertNotNull(result);
+        assertEquals(Response.Status.NOT_FOUND.getStatusCode(), result.getResponseStatus());
+
+    }
+
+
+    @Test(expectedExceptions = ResourceNotFoundException.class)
+    public void should_return_operstionResult_and_throw_ResourceNotFoundException() throws IOException {
+        // Given
+        configuration.setHandleErrors(false);
+        // When
+        OperationResult<InputStream> result = session
+                .thumbnailsService()
+                .thumbnail()
+                .report("/some")
+                .get();
+        result.getEntity();
+        // Then an exception should be thrown
+
+    }
+
+    @Test(expectedExceptions = RequestedRepresentationNotAvailableForResourceException.class)
+    public void should_throw_RequestedRepresentationNotAvailableForResourceException(){
+        // Given
+        configuration.setHandleErrors(true);
+        // When
+        OperationResult<InputStream> result = session
+                .thumbnailsService()
+                .thumbnail()
+                .report("/")
+                .get();
+        // Then an exception should be thrown
+    }
+
+
+    @Test
+    public void should_not_throw_RequestedRepresentationNotAvailableForResourceException_and_return_null_entity() throws IOException {
+        //Given
+        configuration.setHandleErrors(false);
+        // When
+        OperationResult<InputStream> result = session
+                .thumbnailsService()
+                .thumbnail()
+                .report("/")
+                .get();
+        // Then
+        assertNotNull(result);
+        assertEquals(Response.Status.NOT_ACCEPTABLE.getStatusCode(), result.getResponseStatus());
+
+    }
+
+
+    @Test(expectedExceptions = RequestedRepresentationNotAvailableForResourceException.class)
+    public void should_return_operstionResult_and_throw_RequestedRepresentationNotAvailableForResourceException() throws IOException {
+        //Given
+        configuration.setHandleErrors(false);
+        // When
+        OperationResult<InputStream> result = session
+                .thumbnailsService()
+                .thumbnail()
+                .report("/")
+                .get();
+        result.getEntity();
+        // Then an exception should be thrown
+
     }
 
     @AfterClass

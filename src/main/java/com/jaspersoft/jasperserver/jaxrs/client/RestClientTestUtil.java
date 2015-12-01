5577a9fabfd3fbf21ae924de80ceaa8749f8e43b
TanyaEf <efimenkot@gmial.com>
2015-12-01 16:52:26 +0200

    New tests for RoleService, updated dependency

--- src/main/java/com/jaspersoft/jasperserver/jaxrs/client/RestClientTestUtil.java
+++ src/main/java/com/jaspersoft/jasperserver/jaxrs/client/RestClientTestUtil.java
@@ -3,8 +3,10 @@ package com.jaspersoft.jasperserver.jaxrs.client;
 import com.jaspersoft.jasperserver.jaxrs.client.core.JasperserverRestClient;
 import com.jaspersoft.jasperserver.jaxrs.client.core.RestClientConfiguration;
 import com.jaspersoft.jasperserver.jaxrs.client.core.Session;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Properties;
 
 /**
@@ -15,9 +17,10 @@ public abstract class RestClientTestUtil {
     protected JasperserverRestClient client;
     protected Session session;
     protected Properties properties;
+    protected final String PROPERTY_FILE_NAME = "test_config.properties";
 
     protected void initClient() {
-        loadTestProperties("test_config.properties");
+        loadTestProperties(PROPERTY_FILE_NAME);
         configuration = RestClientConfiguration.loadConfiguration(properties);
         client = new JasperserverRestClient(configuration);
     }
@@ -38,6 +41,31 @@ public abstract class RestClientTestUtil {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+
+
+    protected static void streamToFile(InputStream entity, String filename) {
+        OutputStream output = null;
+        try {
+            output = new FileOutputStream(filename);
+            int i = 0;
+            while (i != -1) {
+                i = entity.read();
+                output.write(i);
+                output.flush();
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+            try {
+                entity.close();
+                output.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }

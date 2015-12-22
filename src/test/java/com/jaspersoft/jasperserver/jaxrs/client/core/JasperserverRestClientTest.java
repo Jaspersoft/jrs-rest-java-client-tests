3d62cd05f3c4c9edbf7543d6d3c778cc51a347ab
TanyaEf <efimenkot@gmail.com>
2015-12-22 14:56:56 +0200

    New tsts for ReportService and  authentication

--- src/test/java/com/jaspersoft/jasperserver/jaxrs/client/core/JasperserverRestClientTest.java
+++ src/test/java/com/jaspersoft/jasperserver/jaxrs/client/core/JasperserverRestClientTest.java
@@ -4,16 +4,21 @@ package com.jaspersoft.jasperserver.jaxrs.client.core;
 import com.jaspersoft.jasperserver.jaxrs.client.RestClientTestUtil;
 import com.jaspersoft.jasperserver.jaxrs.client.core.enums.AuthenticationType;
 import com.jaspersoft.jasperserver.jaxrs.client.core.exceptions.JSClientWebException;
+import java.util.TimeZone;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
 /**
  * @author Tetiana Iefimenko
  */
 public class JasperserverRestClientTest extends RestClientTestUtil {
+    private final String USER_NAME = "superuser";
+    private final String PASSWORD = "superuser";
+    private final String USER_TIME_ZONE = "Canada/Central";
 
     @BeforeMethod
     public void before() {
@@ -22,9 +27,17 @@ public class JasperserverRestClientTest extends RestClientTestUtil {
 
     @Test
     public void should_return_session_via_j_sucurity_check() {
-        session = client.authenticate("superuser", "superuser");
+        session = client.authenticate(USER_NAME, PASSWORD);
         assertNotNull(session);
         assertNotNull(session.getStorage().getSessionId());
+    }
+
+    @Test
+    public void should_return_session_via_j_sucurity_check_with_user_time_zone_as_object() {
+        session = client.authenticate(USER_NAME, PASSWORD, TimeZone.getTimeZone(USER_TIME_ZONE));
+        assertNotNull(session);
+        assertNotNull(session.getStorage().getUserTimeZone());
+        assertEquals(USER_TIME_ZONE, session.getStorage().getUserTimeZone().getID());
     }
 
     @Test (expectedExceptions = JSClientWebException.class)

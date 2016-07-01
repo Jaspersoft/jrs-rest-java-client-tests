5a95e8be4ed46a7f5bc212ecdaf92bababa18f24
TanyaEf <efimenkot@gmail.com>
2016-07-01 17:50:29 +0300

    Added PermissionServiceTest

---
+++ src/test/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/permissions/PermissionsServiceTest.java
@@ -0,0 +1,46 @@
+package com.jaspersoft.jasperserver.jaxrs.client.apiadapters.permissions;
+
+import com.jaspersoft.jasperserver.dto.permissions.RepositoryPermission;
+import com.jaspersoft.jasperserver.jaxrs.client.RestClientTestUtil;
+import com.jaspersoft.jasperserver.jaxrs.client.core.operationresult.OperationResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertNotNull;
+
+/**
+ * <p/>
+ * <p/>
+ *
+ * @author tetiana.iefimenko
+ * @version $Id$
+ * @see
+ */
+public class PermissionsServiceTest extends RestClientTestUtil {
+
+    @BeforeClass
+    public void before() {
+        initClient();
+        initSession();
+    }
+
+    @Test
+    public void should_() {
+        OperationResult<RepositoryPermission> operationResult = session
+                .permissionsService()
+                .resource("/organizations/organization_1/datasources")
+                .permissionRecipient(PermissionRecipient.ROLE, "ROLE_USER")
+                .get();
+
+        RepositoryPermission permission = operationResult.getEntity();
+        assertNotNull(permission);
+
+    }
+
+
+    @AfterClass
+    public void after() {
+        session.logout();
+    }
+}

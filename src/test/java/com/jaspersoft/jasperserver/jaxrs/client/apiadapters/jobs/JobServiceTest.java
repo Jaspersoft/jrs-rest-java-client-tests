ab50869f376e314eb9bf53a90d445854ef0af34e
TanyaEf <efimenkot@gmail.com>
2017-07-21 10:40:39 +0300

    JobService tests

--- src/test/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/jobs/JobServiceTest.java
+++ src/test/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/jobs/JobServiceTest.java
@@ -1,16 +1,19 @@
 package com.jaspersoft.jasperserver.jaxrs.client.apiadapters.jobs;
 
+import com.jaspersoft.jasperserver.dto.common.OutputFormat;
+import com.jaspersoft.jasperserver.dto.job.ClientIntervalUnitType;
+import com.jaspersoft.jasperserver.dto.job.ClientJobRepositoryDestination;
+import com.jaspersoft.jasperserver.dto.job.ClientJobSimpleTrigger;
+import com.jaspersoft.jasperserver.dto.job.ClientJobSource;
+import com.jaspersoft.jasperserver.dto.job.ClientReportJob;
+import com.jaspersoft.jasperserver.dto.job.wrappers.ClientJobIdListWrapper;
 import com.jaspersoft.jasperserver.jaxrs.client.RestClientTestUtil;
 import com.jaspersoft.jasperserver.jaxrs.client.core.operationresult.OperationResult;
-import com.jaspersoft.jasperserver.jaxrs.client.dto.jobs.IntervalUnitType;
-import com.jaspersoft.jasperserver.jaxrs.client.dto.jobs.Job;
-import com.jaspersoft.jasperserver.jaxrs.client.dto.jobs.JobSource;
-import com.jaspersoft.jasperserver.jaxrs.client.dto.jobs.OutputFormat;
-import com.jaspersoft.jasperserver.jaxrs.client.dto.jobs.RepositoryDestination;
-import com.jaspersoft.jasperserver.jaxrs.client.dto.jobs.SimpleTrigger;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.testng.annotations.AfterClass;
@@ -35,14 +38,63 @@ public class JobServiceTest extends RestClientTestUtil{
     @Test
     public void should_scheduled_report() {
         // Given
-        Job job = new Job();
+        ClientReportJob job = prepareJob();
+
+        // When
+        OperationResult<ClientReportJob> result = session
+                .jobsService()
+                .scheduleReport(job);
+
+        job = result.getEntity();
+
+        // Then
+        assertNotNull(job.getSource().getParameters());
+        assertEquals(new String[]{"USA"}, job.getSource().getParameters().get("Country_multi_select"));
+        assertEquals(new String[]{"Chin-Lovell Engineering Associates"}, job.getSource().getParameters().get("Cascading_name_single_select"));
+        assertEquals(new String[]{"DF", "Jalisco", "Mexico"}, job.getSource().getParameters().get("Cascading_state_multi_select"));
+    }
+
+    @Test
+    public void should_scheduled_and_delete_reports() {
+        // Given
+        ClientReportJob job = null;
+        List<String> ids = new ArrayList<String>(3);
+
+        for (int i = 0; i < 3; i++) {
+            job = prepareJob();
+            OperationResult<ClientReportJob> result = session
+                    .jobsService()
+                    .scheduleReport(job);
+
+            if (result != null && result.getEntity() != null) {
+                ids.add(String.valueOf(result.getEntity().getId()));
+            }
+        }
+
+        // When
+        OperationResult<ClientJobIdListWrapper> result = session
+                .jobsService()
+                .jobs()
+                .parameters(JobsParameter.JOB_ID, ids.toArray(new String[ids.size()]))
+                .delete();
+        // Then
+        assertNotNull(job.getSource().getParameters());
+        assertEquals(new String[]{"USA"}, job.getSource().getParameters().get("Country_multi_select"));
+        assertEquals(new String[]{"Chin-Lovell Engineering Associates"}, job.getSource().getParameters().get("Cascading_name_single_select"));
+        assertEquals(new String[]{"DF", "Jalisco", "Mexico"}, job.getSource().getParameters().get("Cascading_state_multi_select"));
+
+
+    }
+
+    private ClientReportJob prepareJob() {
+        ClientReportJob job = new ClientReportJob();
         job.setLabel("New Job for ISS Report");
         job.setDescription("New Job for the report template: " + reportUri);
 
-        JobSource jobSource = new JobSource();
+        ClientJobSource jobSource = new ClientJobSource();
         jobSource.setReportUnitURI(reportUri);
 
-        Map<String, Object> parameterValues = new LinkedHashMap<String, Object>();
+        Map<String, String[]> parameterValues = new LinkedHashMap<String, String[]>();
         parameterValues.put("Country_multi_select", new String[]{"USA"});
         parameterValues.put("Cascading_name_single_select", new String[]{"Chin-Lovell Engineering Associates"});
         parameterValues.put("Cascading_state_multi_select", new String[]{"DF", "Jalisco", "Mexico"});
@@ -55,33 +107,21 @@ public class JobServiceTest extends RestClientTestUtil{
         outputFormats.add(OutputFormat.XLSX);
         job.setOutputFormats(outputFormats);
 
-        RepositoryDestination repositoryDestination = new RepositoryDestination();
+        ClientJobRepositoryDestination repositoryDestination = new ClientJobRepositoryDestination();
         repositoryDestination.setSaveToRepository(true);
 
         repositoryDestination.setFolderURI("/organizations/organization_1/adhoc/topics");
         job.setRepositoryDestination(repositoryDestination);
-        SimpleTrigger trigger = new SimpleTrigger();
-        trigger.setStartType(SimpleTrigger.START_TYPE_NOW);
+        ClientJobSimpleTrigger trigger = new ClientJobSimpleTrigger();
+        trigger.setStartType(ClientJobSimpleTrigger.START_TYPE_SCHEDULE);
+        trigger.setStartDate(new GregorianCalendar(2018,1,1,0,0,0).getTime());
         trigger.setOccurrenceCount(1);
         trigger.setRecurrenceInterval(0);
-        trigger.setRecurrenceIntervalUnit(IntervalUnitType.DAY);
+        trigger.setRecurrenceIntervalUnit(ClientIntervalUnitType.DAY);
         job.setTrigger(trigger);
-        job.setBaseOutputFilename("Cascading_multi_select_topic" + System.currentTimeMillis());
+        job.setBaseOutputFilename("Cascading_multi_select_topic" + Math.random());
 
-        // When
-        OperationResult<Job> result = session
-                .jobsService()
-                .scheduleReport(job);
-
-        job = result.getEntity();
-
-        // Then
-        assertNotNull(job.getSource().getParameters());
-        assertEquals(parameterValues.get("Country_multi_select"), ((ArrayList) job.getSource().getParameters().get("Country_multi_select")).toArray());
-        assertEquals(parameterValues.get("Cascading_name_single_select"), ((ArrayList) job.getSource().getParameters().get("Cascading_name_single_select")).toArray());
-        assertEquals(parameterValues.get("Cascading_state_multi_select"), ((ArrayList) job.getSource().getParameters().get("Cascading_state_multi_select")).toArray());
-
-
+        return job;
     }
 
     @AfterClass

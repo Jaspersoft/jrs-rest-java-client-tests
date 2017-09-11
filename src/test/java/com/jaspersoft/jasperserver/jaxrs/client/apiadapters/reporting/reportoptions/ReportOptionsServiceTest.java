b5f7fda6fdbb53a8a9c3302915b1b87a0f6ac5eb
TanyaEf <efimenkot@gmail.com>
2017-09-11 16:14:31 +0300

    tests for ReportOptionsService

---
+++ src/test/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/reporting/reportoptions/ReportOptionsServiceTest.java
@@ -0,0 +1,108 @@
+    package com.jaspersoft.jasperserver.jaxrs.client.apiadapters.reporting.reportoptions;
+
+import com.jaspersoft.jasperserver.dto.reports.options.ReportOptionsSummary;
+import com.jaspersoft.jasperserver.dto.reports.options.ReportOptionsSummaryList;
+import com.jaspersoft.jasperserver.jaxrs.client.RestClientTestUtil;
+import com.jaspersoft.jasperserver.jaxrs.client.core.operationresult.OperationResult;
+import javax.ws.rs.core.MultivaluedHashMap;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.AssertJUnit.assertNotNull;
+
+
+    /**
+     * @author Alex Krasnyanskiy
+     * @author Tetiana Iefimenko
+     */
+    public class ReportOptionsServiceTest extends RestClientTestUtil {
+
+
+        public static final String OPTIONS_LABEL = "new_options";
+        private final String reportUnitUri = "organizations/organization_1/adhoc/topics/Cascading_multi_select_topic";
+
+        @BeforeClass
+        public void before() {
+            initClient();
+            initSession();
+        }
+
+        @Test
+        public void should_return_report_options() {
+
+            // When
+            OperationResult<ReportOptionsSummaryList> result = session
+                    .reportingService()
+                    .report(reportUnitUri)
+                    .reportOptions()
+                    .get();
+
+            final ReportOptionsSummaryList entity = result.getEntity();
+            // Then
+            assertEquals(result.getResponseStatus(), 200);
+        }
+
+        @Test(dependsOnMethods = "should_return_report_options")
+        public void should_create_report_options() {
+            final MultivaluedHashMap<String, String> map = new MultivaluedHashMap<String, String>();
+            map.addAll("Country_multi_select", "Mexico");
+            map.addAll("Cascading_state_multi_select", "Guerrero", "Sinaloa");
+            map.addAll("Cascading_name_single_select", "Crow-Sims Construction Associates");
+
+            // When
+            OperationResult<ReportOptionsSummary> result = session
+                    .reportingService()
+                    .report(reportUnitUri)
+                    .reportOptions(map)
+                    .label(OPTIONS_LABEL)
+                    .create();
+
+            final ReportOptionsSummary entity = result.getEntity();
+            // Then
+            assertEquals(result.getResponseStatus(), 200);
+            assertNotNull(entity);
+            assertEquals(entity.getId(), OPTIONS_LABEL);
+            assertEquals(entity.getLabel(), OPTIONS_LABEL);
+        }
+
+        @Test(dependsOnMethods = "should_create_report_options")
+        public void should_update_report_options() {
+            final MultivaluedHashMap<String, String> map = new MultivaluedHashMap<String, String>();
+            map.addAll("Country_multi_select", "USA");
+
+
+            // When
+            OperationResult<ReportOptionsSummary> result = session
+                    .reportingService()
+                    .report(reportUnitUri)
+                    .reportOptions(OPTIONS_LABEL)
+                    .update(map);
+
+            final ReportOptionsSummary entity = result.getEntity();
+            // Then
+            assertEquals(result.getResponseStatus(), 200);
+        }
+
+        @Test(dependsOnMethods = "should_update_report_options")
+        public void should_delete_report_options() {
+
+            // When
+            OperationResult result = session
+                    .reportingService()
+                    .report(reportUnitUri)
+                    .reportOptions(OPTIONS_LABEL)
+                    .delete();
+
+            // Then
+            assertEquals(result.getResponseStatus(), 200);
+        }
+
+        @AfterClass
+        public void after() {
+            session.logout();
+        }
+
+
+    }
